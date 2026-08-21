aa875bdbdc022edee9d7fec8ad44c6b41cf6a9e1
pajorn <pajorn@small.au>
2026-08-22 08:43:14 +1000

    add registers

--- emulator/Registers.cpp
+++ emulator/Registers.cpp
@@ -1,5 +1,36 @@
 #include "Registers.h"
 
 Registers::Registers() {
-
+    generalPurpose = new uint16_t[16];
 }
+
+void Registers::setGP(uint8_t i, uint16_t val) {
+    if (i >= 16)
+	return;
+
+    generalPurpose[i] = val;
+}
+
+uint16_t Registers::getGP(uint8_t i) {
+    if (i >= 16)
+	return 0;
+
+    return generalPurpose[i];
+}
+
+void Registers::setIP(uint16_t val) {
+    instructionPointer = val;
+}
+
+uint16_t Registers::getIP() {
+    return instructionPointer;
+}
+
+void Registers::setSP(uint16_t val) {
+    stackPointer = val;
+}
+
+uint16_t Registers::getSP() {
+    return stackPointer;
+}
+
--- emulator/Registers.h
+++ emulator/Registers.h
@@ -1,4 +1,20 @@
+#include <cstdint>
+
 class Registers {
 public:
     Registers();
+
+    void setGP(uint8_t i, uint16_t val);
+    uint16_t getGP(uint8_t i);
+
+    void setIP(uint16_t val);
+    uint16_t getIP();
+
+    void setSP(uint16_t val);
+    uint16_t getSP();
+
+private:
+    uint16_t* generalPurpose;
+    uint16_t instructionPointer;
+    uint16_t stackPointer;
 };
