e3a1b212da48c78f10a79841d67676c00d67fc02
pajorn <pajorn@small.au>
2026-08-21 19:53:00 +1000

    create basic scaffolding and makefile

---
+++ emulator/Memory.cpp
@@ -0,0 +1,7 @@
+#include "Memory.h"
+#include <stdio.h>
+
+Memory::Memory() {
+    //constructor
+    printf("memory contructor\n");
+}
---
+++ emulator/main.cpp
@@ -0,0 +1,8 @@
+#include "Memory.h"
+//includes here
+
+int main(void) {
+    Memory* memory = new Memory();
+
+    return 0;
+}
