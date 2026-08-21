7baf46f7f43039a94c8856761f059ea7f52a44c2
Josiah <github@josiahiles.com>
2026-08-21 21:00:36 +1000

    memory header and class

--- emulator/Memory.h
+++ emulator/Memory.h
@@ -1,7 +1,16 @@
 class Memory {
 public:
     Memory();
+    ~Memory();
 
+    Word read(uint16_t address) const;
+    void write(uint16_t address, Word value);
+    void clear();
 private:
+    static const int SIZE = 65536; // 2^16 
+    Word* cells_;
 
+    // NO COPYING
+    Memory(const Memory&) = delete;
+    Memory& operator=(const Memory&) = delete;
 };
