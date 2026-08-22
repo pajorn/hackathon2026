417b8379a68de48fab3b5dfa75f96759b7e439a1
pajorn <pajorn@small.au>
2026-08-22 15:08:59 +1000

    add basic computer functionality

--- emulator/Computer.cpp
+++ emulator/Computer.cpp
@@ -1,5 +1,12 @@
 #include "Computer.h"
 
 Computer::Computer() {
+    decoder = new Decoder();
+    registers = new Registers();
+    memory = new Memory(registers);
+    gpu = new GPU();
+}
 
+void Computer::tick() {
+    decoder->fdeCycle(memory, registers);
 }
--- emulator/Computer.h
+++ emulator/Computer.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include "Decoder.h"
+#include "Memory.h"
+#include "Registers.h"
+#include "GPU.h"
+
 class Computer {
 public:
     Computer();
 
+    void tick();
+
 private:
+    
+
     Decoder* decoder;
     Memory* memory;
     Registers* registers;
