b283e96d4dd90b73b0bafba8e8d98ddc2af99347
pajorn <pajorn@small.au>
2026-08-22 21:43:12 +1000

    makefile

--- emulator/main.cpp
+++ emulator/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char** argv) {
     Computer c;
     if (!c.loadProgram(argv[1]))
-        return 1
+        return 1;
     while (!c.shouldQuit())
         c.tick();
     return 0;
