d43b4d3c527f46c22379adc4a74a9d669d3281b0
pajorn <pajorn@small.au>
2026-08-22 21:46:14 +1000

    fix invalid args crash

--- emulator/main.cpp
+++ emulator/main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char** argv) {
     Computer c;
+    if (argc != 2) {
+	printf("invalid args\n");
+    }
     if (!c.loadProgram(argv[1]))
         return 1;
     while (!c.shouldQuit())
